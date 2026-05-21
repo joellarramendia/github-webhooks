f05a4640733834de722f162ba08278b2177a2853
joellarramendia <joellarramendia@gmail.com>
2026-05-21 17:12:27 -0300

    add GitHubService to handle star and issue events in webhook

--- src/presentation/github/controller.ts
+++ src/presentation/github/controller.ts
@@ -1,12 +1,30 @@
 import { Request, Response } from 'express'
+import { GitHubService } from '../services/github.service.js'
+
 export class GithubController {
-    constructor() { }
+    constructor(
+        private readonly githubService = new GitHubService()
+    ) { }
 
     webhookHandler = (req: Request, res: Response) => {
         const githubEvent = req.header('x-github-event') ?? 'unknow'
         const payload = req.body
+        let message: string
 
         // console.log(JSON.stringify(payload))
+        switch(githubEvent) {
+            case 'star':
+                message = this.githubService.onStar(payload)
+            break
+
+            case 'issues':
+                message = this.githubService.onIssue(payload)
+            break
+
+            default:
+                message = `Unknown event ${githubEvent}`
+        }
+        console.log({message})
 
         res.status(201).send('Accepted')
     }
